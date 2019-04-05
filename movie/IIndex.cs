ec881d0bafa574cffcb22e5b399467c72d771dd1
Raymond Chin <iraychin@gmail.com>
2019-04-05 12:30:58 -0700

    Added interface for Index

---
+++ movie/IIndex.cs
@@ -0,0 +1,19 @@
+﻿public interface IIndex<K, V>
+{
+    void Insert(K key, V Value);
+
+    Tuple<K, V> Get(K key);
+
+    IEnumerable<Tuple<K, V>> LargerThanorEqualTo(K key);
+
+    IEnumberable<Tuple<K, V>> LargerThan(K key);
+
+    IEnumerable<Tuple<K, V>> LessThanOrEqualTo(K key);
+
+    IEnumerable<Tuple<K, V>> LessThan(K key);
+
+    bool Delete(K key, V value, IComparer<V> valueComparer = null);
+
+    bool Delete(K key);
+
+}
